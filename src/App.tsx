f0962e86d53e2a0aa6f294c3d0f41fd5f20ed706
mohamad27911 <mohamad.mar72@gmail.com>
2024-09-18 18:15:58 +0300

    Initial commit with Vite + React and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import './index.css'
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
