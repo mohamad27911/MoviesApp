869e95cf25b3c087c5105787239051879c18fda9
mohamad27911 <mohamad.mar72@gmail.com>
2024-09-20 00:16:28 +0300

    Added The UI libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-
-import './index.css'
-function App() {
+import { Description, Field, Label, Select } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useState } from 'react';
+export default function App() {
+  const [value, setValue] = useState(0);
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    <div className="w-full max-w-md p-4 m-10">
+<p className='text-white text-6xl mb-4'>Headless UI:</p>
+      <Field>
+        <Label className="text-sm/6 font-medium text-white">Project status</Label>
+        <Description className="text-sm/6 text-white/50">This will be visible to clients on the project.</Description>
+        <div className="relative">
+          <Select
+            className={clsx(
+              'mt-3 block w-full appearance-none rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+              'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
+              // Make the text of each option black on Windows
+              '*:text-black'
+            )}
+          >
+            <option value="active">Active</option>
+            <option value="paused">Paused</option>
+            <option value="delayed">Delayed</option>
+            <option value="canceled">Canceled</option>
+          </Select>
+          <ChevronDownIcon
+            className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
+            aria-hidden="true"
+          />
+        </div>
+      </Field>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p className='text-white text-6xl mb-4'>Material UI:</p>
+      <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
     </div>
-  );
+  )
 }
-
-export default App;
