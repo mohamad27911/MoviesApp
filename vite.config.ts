f4a8e4608fceaf1ed54c9b850cb01739741fcd2f
mohamad27911 <mohamad.mar72@gmail.com>
2024-10-22 17:55:20 +0300

    fix index

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MoviesApp/', // Update this to match your GitHub repository name
   plugins: [react()],
-  base: '/MoviesApp/', // Set the base path to match your GitHub repository
-})
+});
